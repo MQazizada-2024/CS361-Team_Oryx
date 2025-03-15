b72b77278a803603c53ff23a92bbb2ffc7daf207
MQazizada-2024 <mqazizada@eou.edu>
2025-03-14 23:29:47 -0700

    Update Flashcard.js
    Fixed bugs

--- JavaScript/Flashcard.js
+++ JavaScript/Flashcard.js
@@ -1,27 +1,29 @@
 class Flashcard {
-    constructor(id, question, answer, contentType='text', tags =[]){
+    constructor(id, question, answer, contentType = 'text', tags = []) {
         this.id = id;
         this.question = question;
         this.answer = answer;
         this.contentType = contentType;
-        this.tags = tags || []; 
+        this.tags = tags || [];
     }
-    
-    editQuestion(newQuestion){
-        this.qustion = newQuestion;
+
+    editQuestion(newQuestion) {
+        this.question = newQuestion; // Fixed typo
     }
-    editAnswer(newAnswer){
+
+    editAnswer(newAnswer) {
         this.answer = newAnswer;
     }
-    addTag(tag){
-        if (!this.tags.includes(tag)){
-            this.tags.push(tag)
+
+    addTag(tag) {
+        if (!this.tags.includes(tag)) {
+            this.tags.push(tag);
         }
     }
-    removeTag(tag){
+
+    removeTag(tag) {
         this.tags = this.tags.filter(t => t !== tag);
     }
-
 }
-module.exports = Flashcard;
 
+module.exports = Flashcard;
