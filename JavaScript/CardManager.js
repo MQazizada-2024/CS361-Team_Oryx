051ff4708d04e7da1c6d3cb400c3a52bcd86a205
MQazizada-2024 <mqazizada@eou.edu>
2025-03-14 23:28:49 -0700

    Update CardManager.js
    Fixed some bugs

--- JavaScript/CardManager.js
+++ JavaScript/CardManager.js
@@ -1,56 +1,62 @@
-class CardManager{
-    constructor(currentDeck = [], currentIndex = 0){
+class CardManager {
+    constructor(currentDeck = [], currentIndex = 0) {
         this.currentDeck = currentDeck;
         this.currentIndex = currentIndex;
         this.isFlipped = false;
         this.isSessionActive = false;
     }
-    startSession(deck){
-        if(!deck || deck.length === 0){
+
+    startSession(deck) {
+        if (!deck || !deck.flashcards || deck.flashcards.length === 0) { 
             console.log("The deck is empty.");
             return;
         }
-        this.currentDeck = deck;
+        this.currentDeck = deck.flashcards; // Assign the flashcards array
         this.currentIndex = 0;
         this.isFlipped = false;
         this.isSessionActive = true;
-        console.log("Session started.")
+        console.log("Session started.");
     }
-    nextFlashcard(){
-        if(!this.isSessionActive){
+
+    nextFlashcard() {
+        if (!this.isSessionActive) {
             console.log("Start session first.");
             return;
         }
-        if (this.currentIndex < this.currentDeck.length - 1){
+        if (this.currentIndex < this.currentDeck.length - 1) {
             this.currentIndex++;
             this.isFlipped = false;
-        }else {
+        } else {
             console.log("You have reached the end of the deck.");
         }
     }
-    previousFlashcard(){
-            if(!this.isSessionActive){
-                console.log("Start session first.");
-                return;
-            }
-            if (this.currentIndex > 0){
-                this.currentIndex--;
-                this.isFlipped = false;
-            } else{
-                console.log("You are at the beginnig of the deck.")
-            }
+
+    previousFlashcard() {
+        if (!this.isSessionActive) {
+            console.log("Start session first.");
+            return;
+        }
+        if (this.currentIndex > 0) {
+            this.currentIndex--;
+            this.isFlipped = false;
+        } else {
+            console.log("You are at the beginning of the deck."); // Fixed typo
+        }
     }
-    flipFlashcard(){
-        if(!this.isSessionActive){
+
+    flipFlashcard() {
+        if (!this.isSessionActive) {
             console.log("Start session first.");
             return;
         }
         this.isFlipped = !this.isFlipped;
     }
-    resetSession(){
+
+    resetSession() {
         this.currentIndex = 0;
         this.isFlipped = false;
         console.log("Session reset.");
     }
 }
-mudule.exports = CardManager;
+
+module.exports = CardManager;
